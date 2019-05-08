fda706e52667aa264e53b185b0f6d983c44ce37d
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-08 14:54:48 +0900

    Add GlobalFooter component

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,43 @@
     <router-link to="/leagues">Leagues</router-link>
     <router-link to="/weapons">Weapon popularity</router-link> |
     <router-view></router-view>
+
+    <global-footer></global-footer>
   </div>
 </template>
 
 <script>
+import GlobalFooter from './components/GlobalFooter.vue';
+
 export default {
   name: 'app',
+  components: { GlobalFooter },
 };
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  position: relative;
+  min-height: 100vh;
   max-width: 1200px;
   margin: 0 auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-bottom: 3em; /* footer height */
+  box-sizing: border-box;
+}
+
+.global-footer {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  height: 3em;
 }
 </style>
---
+++ src/components/GlobalFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="global-footer">
+    GitHub |
+    Twitter @Yukinkling
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooter',
+};
+</script>
+
+<style scoped>
+.global-footer {
+  text-align: center;
+}
+</style>
