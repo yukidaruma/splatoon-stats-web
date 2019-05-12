d991aa33f97859b24d187fee7a07d9e0174520be
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-12 22:27:38 +0900

    Add proper implementation for DatePicker.vue

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,18 +1,67 @@
 <template>
   <div>
     <select v-model="year" @change="emitYearChange">
-      <option :value="yearOption.year" v-for="yearOption in yearOptions" :key="yearOption.key" :selected="yearOption.year === year">{{ yearOption.year }}</option>
+      <option :value="yearOption" v-for="yearOption in yearOptions" :key="yearOption"
+        :selected="yearOption === year">
+        {{ yearOption }}
+      </option>
     </select>
     <select v-model="month" @change="emitMonthChange">
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
+      <option v-for="monthOption in monthOptions" :value="monthOption" :key="monthOption"
+        :selected="monthOption === month">
+        {{ monthOption }}
+      </option>
     </select>
     <!-- Todo: change depending on rankingType ({{ rankingType }}) -->
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
+const getYearOptions = (startingYear) => {
+  return {
+    [Symbol.iterator]() {
+      const currentYear = moment.utc().year();
+      let year = startingYear - 1;
+
+      const iterator = {
+        next() {
+          year += 1;
+          if (year <= currentYear) {
+            return { value: year, done: false };
+          }
+          return { value: undefined, done: true };
+        },
+      };
+      return iterator;
+    },
+  };
+};
+
+const getMonthOptions = (startingYear, startingMonth, includeThisMonth = false) => {
+  return {
+    [Symbol.iterator]() {
+      const now = moment.utc();
+      const currentYear = now.year();
+      const currentMonth = now.month() + 1;
+      const thresholdMonth = includeThisMonth ? currentMonth : currentMonth - 1;
+      let month = startingMonth ? startingMonth - 1 : 0;
+
+      const iterator = {
+        next() {
+          month += 1;
+          if (month > 12 || (startingYear === currentYear && month > thresholdMonth)) {
+            return { value: undefined, done: true };
+          }
+          return { value: month, done: false };
+        },
+      };
+      return iterator;
+    },
+  };
+};
+
 export default {
   name: 'LeagueDatePicker',
   props: ['rankingType', 'defaultYear', 'defaultMonth'],
@@ -21,12 +70,20 @@ export default {
       year: undefined,
       month: undefined,
       yearOptions: [],
+      monthOptions: [],
     };
   },
   created() {
     this.year = this.defaultYear;
     this.month = this.defaultMonth;
     this.updateDatePicker();
+
+    this.$watch(
+      () => [this.$data.year, this.$data.month],
+      (newYear) => {
+        this.updateDatePicker(newYear);
+      },
+    );
   },
   methods: {
     emitYearChange() {
@@ -36,13 +93,20 @@ export default {
       this.$emit('month-change', this.month);
     },
     updateDatePicker() {
-      if (this.rankingType === 'x') {
-        this.yearOptions = [2017, 2018, 2019].map(year => ({ year, key: year }));
-      } else { // league
+      const startingMonth = this.rankingType === 'x' && this.year === 2018 ? 5
+                          : this.rankingType === 'league' && this.year === 2017 ? 7
+                          : 1;
+      const monthOptions = Array.from(getMonthOptions(this.year, startingMonth, this.rankingType === 'league'));
 
+      // Note that it should be getYearOptions(2017) for league.
+      this.yearOptions = getYearOptions(2018);
+      this.monthOptions = monthOptions;
+
+      // Set to last month of if the option is unavailable
+      if (!monthOptions.includes(this.month)) {
+        this.month = monthOptions[monthOptions.length - 1];
+        this.emitMonthChange();
       }
-
-      // If out of date, set to latest available date
     },
   },
 };
