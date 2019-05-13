47b43f55c017b1e9f22f3b552b4d9af88a10439e
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-13 09:33:17 +0900

    Add date and hour selector

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -12,6 +12,21 @@
         {{ monthOption }}
       </option>
     </select>
+
+    <div v-if="rankingType === 'league'">
+      <select v-model="date" @change="emitDateChange">
+        <option v-for="dateOption in dateOptions" :value="dateOption" :key="dateOption"
+          :selected="dateOption === date">
+          {{ dateOption }}
+        </option>
+      </select>
+      <select v-model="hour" @change="emitHourChange">
+        <option v-for="hourOption in hourOptions" :value="hourOption" :key="hourOption"
+          :selected="hourOption === hour">
+          {{ hourOption }}
+        </option>
+      </select>
+    </div>
     <!-- Todo: change depending on rankingType ({{ rankingType }}) -->
   </div>
 </template>
@@ -19,93 +34,174 @@
 <script>
 import moment from 'moment';
 
-const getYearOptions = (startingYear) => {
-  return {
-    [Symbol.iterator]() {
-      const currentYear = moment.utc().year();
-      let year = startingYear - 1;
+const getDatePickerOptions = (starting, ending, year, month, date, hour) => {
+  const startingYear = starting.year();
+  const startingMonth = starting.month();
+  const startingDate = starting.date();
 
+  const yearOptions = {
+    [Symbol.iterator]() {
+      let yearOption = startingYear - 1;
       const iterator = {
         next() {
-          year += 1;
-          if (year <= currentYear) {
-            return { value: year, done: false };
+          yearOption += 1;
+          if (moment({ year: yearOption}) > ending) {
+            return { value: undefined, done: true };
           }
-          return { value: undefined, done: true };
+          return { value: yearOption, done: false };
         },
       };
       return iterator;
     },
   };
-};
 
-const getMonthOptions = (startingYear, startingMonth, includeThisMonth = false) => {
-  return {
+  const monthOptions = {
     [Symbol.iterator]() {
-      const now = moment.utc();
-      const currentYear = now.year();
-      const currentMonth = now.month() + 1;
-      const thresholdMonth = includeThisMonth ? currentMonth : currentMonth - 1;
-      let month = startingMonth ? startingMonth - 1 : 0;
-
+      let monthOption = year === startingYear ? startingMonth : 0;
       const iterator = {
         next() {
-          month += 1;
-          if (month > 12 || (startingYear === currentYear && month > thresholdMonth)) {
+          monthOption += 1;
+          if (moment.utc({ year, month: monthOption - 1 }) > ending || monthOption > 12) {
             return { value: undefined, done: true };
           }
-          return { value: month, done: false };
+          return { value: monthOption, done: false };
         },
       };
       return iterator;
     },
+  };
+
+  const dateOptions = {
+    [Symbol.iterator]() {
+      const lastDateOfMonth = moment.utc({ year, month: month - 1 }).endOf('month').date();
+      let dateOption = year === startingYear && month === startingMonth ? startingDate - 1 : 0;
+
+      const iterator = {
+        next() {
+          dateOption += 1;
+          if (moment.utc({ year, month: month - 1, date: dateOption }) > ending || dateOption > lastDateOfMonth) {
+            return { value: undefined, done: true };
+          }
+          return { value: dateOption, done: false };
+        },
+      };
+      return iterator;
+    },
+  };
+
+  const hourOptions = {
+    [Symbol.iterator]() {
+      let hourOption = -2;
+
+      const iterator = {
+        next() {
+          hourOption += 2;
+          if (moment.utc({ year, month: month - 1, date, hour: hourOption }) > ending || hourOption >= 24) {
+            return { value: undefined, done: true };
+          }
+          return { value: hourOption, done: false };
+        },
+      };
+      return iterator;
+    },
+  };
+
+  return {
+    yearOptions: Array.from(yearOptions),
+    monthOptions: Array.from(monthOptions),
+    dateOptions: Array.from(dateOptions),
+    hourOptions: Array.from(hourOptions),
   };
 };
 
 export default {
   name: 'LeagueDatePicker',
-  props: ['rankingType', 'defaultYear', 'defaultMonth'],
+  props: ['rankingType', 'defaultYear', 'defaultMonth', 'defaultDate', 'defaultHour'],
   data() {
     return {
       year: undefined,
       month: undefined,
+      date: undefined,
+      hour: undefined,
       yearOptions: [],
       monthOptions: [],
+      dateOptions: [],
+      hourOptions: [],
     };
   },
   created() {
-    this.year = this.defaultYear;
-    this.month = this.defaultMonth;
+    let defaultDate = moment.utc({ year: this.defaultYear, month: this.defaultMonth - 1, date: this.defaultDate, hour: this.defaultHour });
+    this.updateSelectedDate(defaultDate);
     this.updateDatePicker();
 
     this.$watch(
-      () => [this.$data.year, this.$data.month],
+      // You don't have to watch hour because it won't change options.
+      () => [this.$data.year, this.$data.month, this.$data.date],
       (newYear) => {
-        this.updateDatePicker(newYear);
+        this.updateDatePicker();
       },
     );
   },
   methods: {
-    emitYearChange() {
-      this.$emit('year-change', this.year);
-    },
-    emitMonthChange() {
-      this.$emit('month-change', this.month);
+    emitYearChange() { this.$emit('year-change', this.year); },
+    emitMonthChange() { this.$emit('month-change', this.month); },
+    emitDateChange() { this.$emit('date-change', this.date); },
+    emitHourChange() { this.$emit('hour-change', this.hour); },
+    updateSelectedDate(newSelectedDate) {
+      const now = moment.utc();
+
+      if (newSelectedDate > now) {
+        newSelectedDate = now;
+      }
+
+      this.year = newSelectedDate.year();
+      this.month = newSelectedDate.month() + 1;
+      this.date = newSelectedDate.date();
+      this.hour = 0;
     },
     updateDatePicker() {
-      const startingMonth = this.rankingType === 'x' && this.year === 2018 ? 5
-                          : this.rankingType === 'league' && this.year === 2017 ? 7
-                          : 1;
-      const monthOptions = Array.from(getMonthOptions(this.year, startingMonth, this.rankingType === 'league'));
+      let startingYear, startingMonth, startingDate, ending;
+      const now = moment.utc();
 
-      // Note that it should be getYearOptions(2017) for league.
-      this.yearOptions = getYearOptions(2018);
+      if (this.rankingType === 'x') {
+        startingYear = 2018;
+        startingMonth = 5;
+        ending = now.add({ month: -1 });
+      } else {
+        // Should be 2017/7/21
+        startingYear = 2018;
+        startingMonth = 1;
+        startingDate = 1;
+        ending = now;
+      }
+
+      const starting = moment.utc({ year: startingYear, month: startingMonth - 1, date: startingDate });
+
+      const datePickerOptions = getDatePickerOptions(starting, ending, this.year, this.month, this.date);
+      const { yearOptions, monthOptions, dateOptions, hourOptions } = datePickerOptions;
+      this.yearOptions = yearOptions;
       this.monthOptions = monthOptions;
+      this.dateOptions = dateOptions;
+      this.hourOptions = hourOptions;
 
-      // Set to last month of if the option is unavailable
+      // Update selected date to latest available date if not available
       if (!monthOptions.includes(this.month)) {
-        this.month = monthOptions[monthOptions.length - 1];
-        this.emitMonthChange();
+        this.updateSelectedDate(moment.utc({
+          year: this.year,
+          month: monthOptions[monthOptions.length - 1],
+        }));
+      } else if (!dateOptions.includes(this.date)) {
+        this.updateSelectedDate(moment.utc({
+          year: this.year,
+          month: this.month - 1,
+          date: dateOptions[dateOptions.length - 1],
+        }));
+      } else if (!hourOptions.includes(this.hour)) {
+        this.updateSelectedDate(moment.utc({
+          year: this.year,
+          month: this.month - 1,
+          date: this.date,
+        }));
       }
     },
   },
