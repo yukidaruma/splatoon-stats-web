439e53457eb234bcab6431f3a6f69008d157c6d2
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-20 12:13:27 +0900

    Update Index.vue to show dataset stats

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    Index
+    <h1>Splatoon Stats</h1>
+    <h2>Dataset</h2>
+    <p>X Ranked Battles: <strong>{{ stats.x_rankings * 2000 }}</strong> of {{ stats.x_rankings }} months</p>
+    <p>League Battles: <strong>{{ stats.league_rankings_estimate }}</strong> <small>(estimated; smaller than actual value)</small></p>
+    <p>Splatfests: <strong>{{ stats.splatfests }}</strong></p>
   </div>
 </template>
 
 <script>
+import apiClient from '../api-client';
+
 export default {
   name: 'Index',
+  data() {
+    return {
+      stats: {},
+    };
+  },
+  created() {
+    apiClient.get('/stats')
+      .then((res) => {
+        this.stats = res.data;
+      });
+  },
 };
 </script>
 
