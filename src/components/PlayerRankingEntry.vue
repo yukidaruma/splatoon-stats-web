5e3ff4a2b805610a02c631fffe8366a7c09b44e1
yukidaruma <yukidaruma@users.noreply.github.com>
2019-05-22 17:53:00 +0900

    Add date format for splatfest

--- src/components/PlayerRankingEntry.vue
+++ src/components/PlayerRankingEntry.vue
@@ -53,7 +53,7 @@ export default {
   props: ['playerName', 'rankingEntry', 'rankingType'],
   filters: {
     formatDate(time, rankingType) {
-      const dateFormat = { x: 'YY-MM', league: 'YY-MM-DD HH:mm' }[rankingType];
+      const dateFormat = { x: 'YY-MM', league: 'YY-MM-DD HH:mm', splatfest: 'YYYY-MM-DD' }[rankingType];
       return moment.utc(time).local().format(dateFormat);
     },
   },
