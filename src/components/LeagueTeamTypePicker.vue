0f1c75244b2f003c12f73cd978001b6e3f74e88b
yukidaruma <yukidaruma@users.noreply.github.com>
2020-01-04 14:36:03 +0900

    Add team/pair filter to league rankings in PlayerSummary
    Close #19

---
+++ src/components/LeagueTeamTypePicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <select v-model="teamType" @change="$emit('input', teamType)">
+    <option v-for="(v, k) in LeagueTeamTypes" :key="k" :value="v">
+      {{$t(`ui.team_types.${k}`)}}
+    </option>
+  </select>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export const LeagueTeamTypes = {
+  all: 1,
+  team: 2,
+  pair: 3,
+};
+
+export default {
+  name: 'LeagueTeamTypePicker',
+  props: ['value'],
+  data() {
+    return {
+      teamType: this.value,
+      LeagueTeamTypes,
+    };
+  },
+};
+</script>
