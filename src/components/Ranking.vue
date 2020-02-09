b6fc62f2ca48fbb76505f5a3de1fa37e52f15450
yukidaruma <yukidaruma@users.noreply.github.com>
2020-02-09 23:21:15 +0900

    Add total records message in Ranking

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -5,42 +5,46 @@
     </div>
     <div v-else>
       <p v-if="ranking.length === 0">No data found.</p>
-      <table class="table is-fullwidth is-striped is-hoverable" v-else>
-        <tbody>
-          <tr v-for="rankingEntry in filteredRanking" :key="rankingEntry.rank">
-            <td>#{{ rankingEntry.rank }}</td>
-            <td>{{ rankingEntry.rating }}</td>
-            <td>
-              <div v-if="rankingType === 'x' || rankingType === 'splatfest'">
-                <div class="weapon-name-container">
-                  <img class="weapon-icon" :src="rankingEntry.icon">
-                  <router-link :to="`/players/${rankingEntry.player_id}`">
-                    <span class="player-name" v-if="'player_name' in rankingEntry">
-                      {{ rankingEntry.player_name }}
-                    </span>
-                    <span class="player-id" v-else>
-                      {{ rankingEntry.player_id }}
-                    </span>
-                  </router-link>
+      <div v-else>
+        <p v-if="weaponFilter">{{ filteredRanking.length }} records ({{ranking.length}} before filter)</p>
+        <p v-else>{{ ranking.length }} records</p>
+        <table class="table is-fullwidth is-striped is-hoverable">
+          <tbody>
+            <tr v-for="rankingEntry in filteredRanking" :key="rankingEntry.rank">
+              <td>#{{ rankingEntry.rank }}</td>
+              <td>{{ rankingEntry.rating }}</td>
+              <td>
+                <div v-if="rankingType === 'x' || rankingType === 'splatfest'">
+                  <div class="weapon-name-container">
+                    <img class="weapon-icon" :src="rankingEntry.icon">
+                    <router-link :to="`/players/${rankingEntry.player_id}`">
+                      <span class="player-name" v-if="'player_name' in rankingEntry">
+                        {{ rankingEntry.player_name }}
+                      </span>
+                      <span class="player-id" v-else>
+                        {{ rankingEntry.player_id }}
+                      </span>
+                    </router-link>
+                  </div>
                 </div>
-              </div>
-              <div class="league-members" v-if="rankingType === 'league'">
-                <div class="league-member weapon-name-container" v-for="member in rankingEntry.group_members" :key="member.player_id">
-                  <img class="weapon-icon" :src="weaponIcon('weapons', member[1])">
-                  <router-link :to="`/players/${member[0]}`">
-                    <span class="player-name" v-if="member[2]">
-                      {{ member[2] }}
-                    </span>
-                    <span class="player-id" v-else>
-                      {{ member[0] }}
-                    </span>
-                  </router-link>
+                <div class="league-members" v-if="rankingType === 'league'">
+                  <div class="league-member weapon-name-container" v-for="member in rankingEntry.group_members" :key="member.player_id">
+                    <img class="weapon-icon" :src="weaponIcon('weapons', member[1])">
+                    <router-link :to="`/players/${member[0]}`">
+                      <span class="player-name" v-if="member[2]">
+                        {{ member[2] }}
+                      </span>
+                      <span class="player-id" v-else>
+                        {{ member[0] }}
+                      </span>
+                    </router-link>
+                  </div>
                 </div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
