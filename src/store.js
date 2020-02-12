c1600909a14c46e345affa497e585d4dd646a7e8
yukidaruma <yukidaruma@users.noreply.github.com>
2020-02-12 16:18:34 +0900

    Add `<favorite-button>` in PlayerSummary
    Part of #28

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import apiClient from './api-client';
+import storejs from 'store';
 
 Vue.use(Vuex);
 
+const favoritePlayers = storejs.get('favorite-players', []);
+const saveFavoritePlayers = () => storejs.set('favorite-players', favoritePlayers);
+
 const store = new Vuex.Store({
   state: {
+    favoritePlayers,
     weapons: [],
   },
   mutations: {
@@ -19,6 +24,17 @@ const store = new Vuex.Store({
 
       const { data } = await apiClient.get('/data');
       context.commit('SET_WEAPONS', data.weapons);
+    },
+    addFavoritePlayer(context, player) {
+      favoritePlayers.push(player);
+
+      saveFavoritePlayers();
+    },
+    removeFavoritePlayer(context, playerToRemove) {
+      const i = favoritePlayers.findIndex(playerId => playerId === playerToRemove.id);
+      favoritePlayers.splice(i, 1);
+
+      saveFavoritePlayers();
     },
   },
 });
