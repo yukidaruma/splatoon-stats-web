70a7a1deed9860dbbb23dfcb7e3a0c5e643b5b70
yukidaruma <yukidaruma@users.noreply.github.com>
2020-12-19 12:50:30 +0900

    chore: Remove files except for i18n

--- src/api-client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const apiBaseUrl = VUE_APP_API_URL; // eslint-disable-line no-undef
-const client = axios.create({
-  baseURL: apiBaseUrl,
-  responseType: 'json',
-});
-
-export default client;
